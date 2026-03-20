217c09b36dcb2fc925ae544eec744a3ab6ec77a8
Dejan-afk <dejanfra@web.de>
2026-03-20 09:48:05 +0100

    added forms for seminar creation

---
+++ src/Form/SeminarType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Dto\SeminarCreateDto;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeminarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('startDate', DateTimeType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+            ])
+            ->add('endDate', DateTimeType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+            ])
+            ->add('registrationDeadline', DateTimeType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+            ])
+            ->add('maxParticipants', IntegerType::class)
+            ->add('sessions', CollectionType::class, [
+                'entry_type' => SessionType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'prototype' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SeminarCreateDto::class,
+            'csrf_protection' => true,
+        ]);
+    }
+}
